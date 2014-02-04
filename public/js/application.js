39f135eac39b269b00f031681f9232ce46d28db4
Promoveo <zappeln@gmail.com>
2014-02-03 20:42:21 -0600

    basic racer running

--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,43 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  console.log("Yay ready.");
+  var p1wins = 0;
+  var p2wins = 0;
+  var pos1 = 1;
+  var pos2 = 1;
+
+  $(document).on('keyup', function(e) {
+    console.log("Yay, keypress.");
+    if (e.which == 81) {
+      console.log("q pressed");
+      $("#player1_strip td:nth-child(" + pos1 + ")").removeClass("active");
+      pos1++;
+      $("#player1_strip td:nth-child(" + pos1 + ")").addClass("active");
+      if (pos1 >= 40) {
+        p1wins ++;
+        alert("Player 1 wins!!!  Player 1 has won " + p1wins + " times.");
+        pos1 = 1;
+        pos2 = 1;
+        $("td").removeClass("active");
+        $("td:nth-child(" + pos1 + ")").addClass("active");
+      }
+    }
+    else if (e.which == 80) {
+      console.log("p pressed")
+      $("#player2_strip td:nth-child(" + pos2 + ")").removeClass("active");
+      pos2 ++;
+      $("#player2_strip td:nth-child(" + pos2 + ")").addClass("active");
+      if (pos2 >= 40) {
+        p2wins ++;
+        alert("Player 2 wins!!! Player 2 has won " + p2wins + " times.");
+        pos1 = 1;
+        pos2 = 1;
+        $("td").removeClass("active");
+        $("td:nth-child(" + pos1 + ")").addClass("active");
+      }
+    }
+    e.preventDefault();
+  });
+
+
 });
